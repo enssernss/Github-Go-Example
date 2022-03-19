8c28debba192f362318282ebe20f5ffeb41565c1
Enis ERENLER <eniserenler@Enis-MacBook-Pro.local>
2022-03-19 19:11:22 +0300

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,11 @@
+// We Are Setting Package For Make File Executable Here:
 package main
 
+// We Are Importing fmt Library For Use Basic Codes Here:
 import "fmt"
 
+// Program Executes This Fuction:
 func main() {
+	// We Are Printing Hello World To Screen Here:
 	fmt.Println("Hello World")
 }
